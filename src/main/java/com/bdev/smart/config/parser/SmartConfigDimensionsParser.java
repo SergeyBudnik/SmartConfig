65495b19750f4b1f07b1a74482231d6e92c97064
Sergey <serbudnik@gmail.com>
2017-05-06 13:59:33 +0300

    dimensions test

--- src/main/java/com/bdev/smart/config/parser/SmartConfigDimensionsParser.java
+++ src/main/java/com/bdev/smart/config/parser/SmartConfigDimensionsParser.java
@@ -4,6 +4,7 @@ import com.bdev.smart.config.data.inner.DimensionInfo;
 import com.bdev.smart.config.reader.SmartConfigReader;
 import com.typesafe.config.ConfigFactory;
 import com.typesafe.config.ConfigList;
+import com.typesafe.config.ConfigValueType;
 
 import java.io.File;
 import java.util.HashMap;
@@ -18,14 +19,41 @@ public class SmartConfigDimensionsParser {
         SmartConfigReader
                 .read(filePath)
                 .forEach(dimension -> {
-                    // ToDo: validation
+                    if (res.containsKey(dimension.getKey())) {
+                        throw new RuntimeException();
+                    }
 
                     res.put(dimension.getKey(), new DimensionInfo());
 
-                    for (Object value : ((ConfigList) dimension.getValue()).unwrapped()) {
-                        res.get(dimension.getKey()).getDimensions().add((String) value);
+                    if (dimension.getValue().valueType() != ConfigValueType.LIST) {
+                        throw new RuntimeException();
+                    }
+
+                    Set<String> allDimensions = res.get(dimension.getKey()).getDimensions();
+
+                    for (Object rawValue : ((ConfigList) dimension.getValue()).unwrapped()) {
+                        String value = (String) rawValue;
+
+                        if (allDimensions.contains(value)) {
+                            throw new RuntimeException();
+                        }
+
+                        allDimensions.add(value);
                     }
                 });
+
+        long totalAmount = res.values()
+                .stream()
+                .count();
+
+        long distinctAmount = res.values()
+                .stream()
+                .distinct()
+                .count();
+
+        if (totalAmount != distinctAmount) {
+            throw new RuntimeException();
+        }
 
         return res;
     }
--- src/test/java/com/bdev/smart/config/parser/SmartConfigDimensionsParserTest.java
+++ src/test/java/com/bdev/smart/config/parser/SmartConfigDimensionsParserTest.java
@@ -1,10 +1,76 @@
 package com.bdev.smart.config.parser;
 
+import com.bdev.smart.config.data.inner.DimensionInfo;
+import jdk.nashorn.internal.ir.annotations.Ignore;
 import org.junit.Test;
+
+import java.io.File;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class SmartConfigDimensionsParserTest {
     @Test
     public void testSingleDimensionSingleValue() {
-        
+        Map<String, DimensionInfo> dimensionsInfo = SmartConfigDimensionsParser.parse(
+                new File("src/test/resources/dimensions-parser/test-dimension-single-value-single.conf").getPath()
+        );
+
+        assertEquals(1, dimensionsInfo.size());
+        assertEquals(1, dimensionsInfo.get("tier").getDimensions().size());
+        assertTrue(dimensionsInfo.get("tier").getDimensions().contains("sit"));
+    }
+
+    @Test
+    public void testSingleDimensionMultipleValues() {
+        Map<String, DimensionInfo> dimensionsInfo = SmartConfigDimensionsParser.parse(
+                new File("src/test/resources/dimensions-parser/test-dimension-single-values-multiple.conf").getPath()
+        );
+
+        assertEquals(1, dimensionsInfo.size());
+        assertEquals(2, dimensionsInfo.get("tier").getDimensions().size());
+        assertTrue(dimensionsInfo.get("tier").getDimensions().contains("sit"));
+        assertTrue(dimensionsInfo.get("tier").getDimensions().contains("uat"));
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void testSingleDimensionMultipleValuesConflict() {
+        SmartConfigDimensionsParser.parse(
+                new File("src/test/resources/dimensions-parser/test-dimension-single-values-multiple-conflict.conf").getPath()
+        );
+    }
+
+    @Test
+    public void testMultipleDimensionsSingleValue() {
+        Map<String, DimensionInfo> dimensionsInfo = SmartConfigDimensionsParser.parse(
+                new File("src/test/resources/dimensions-parser/test-dimensions-multiple-value-single.conf").getPath()
+        );
+
+        assertEquals(2, dimensionsInfo.size());
+
+        assertEquals(1, dimensionsInfo.get("tier").getDimensions().size());
+        assertTrue(dimensionsInfo.get("tier").getDimensions().contains("sit"));
+
+        assertEquals(1, dimensionsInfo.get("zone").getDimensions().size());
+        assertTrue(dimensionsInfo.get("zone").getDimensions().contains("uk"));
+    }
+
+    /**
+     * Equal keys are ignored by typesafe config implementation
+     */
+    @Test(expected = RuntimeException.class)
+    @Ignore
+    public void testMultipleDimensionsSingleValueNamesConflict() {
+        SmartConfigDimensionsParser.parse(
+                new File("src/test/resources/dimensions-parser/test-dimensions-multiple-value-single-names-conflict.conf").getPath()
+        );
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void testMultipleDimensionsSingleValueValuesConflict() {
+        SmartConfigDimensionsParser.parse(
+                new File("src/test/resources/dimensions-parser/test-dimensions-multiple-value-single-values-conflict.conf").getPath()
+        );
     }
 }
